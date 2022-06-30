6253aa9f34b47bf5a6cb5e278a92b60e319d832d
ArisuXD <96303829+ArisuXD@users.noreply.github.com>
2022-06-30 19:08:05 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,12 @@
-echo "Installing Requirements..."
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /Lisa-filter-Beta
+fi
+cd /Lisa-filter-Beta
 pip3 install -U -r requirements.txt
-echo "Starting Bot, Please Wait..."
+echo "Starting Bot...."
 python3 bot.py
